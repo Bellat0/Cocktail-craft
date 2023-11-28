d61bfbbcf55e89932a6cfdb4c9e05b193b976b0a
MaximTvilinev <maximtvilinev@gmail.com>
2023-11-28 18:42:30 +0600

    Initial Commit

---
+++ CocktailRecipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocktailRecipes
+//
+//  Created by Maxim Tvilinev on 28.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
